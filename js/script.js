6431b57e8f932b52f526e772a1b2382498a2c1cd
Atif-AI <163405149+Codewith-Atif@users.noreply.github.com>
2026-07-08 00:27:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Portfolio Loaded Successfully");
+window.addEventListener("scroll",function(){
+
+const header=document.querySelector("header");
+
+if(window.scrollY>50){
+
+header.style.background="#0B1120";
+
+header.style.boxShadow="0 5px 25px rgba(0,0,0,.3)";
+
+}
+
+else{
+
+header.style.background="#111827";
+
+header.style.boxShadow="none";
+
+}
+
+});
